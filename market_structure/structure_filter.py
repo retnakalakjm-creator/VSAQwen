6e108199371455d63215258b7438474797bc5ebc
retnakalakjm-creator <retnakalakjm@gmail.com>
2026-09-02 19:11:56 +0530

    reverted

--- market_structure/structure_filter.py
+++ market_structure/structure_filter.py
@@ -32,8 +32,7 @@ class StructureFilter:
         swing_tuple = tuple(swings)
         metric_indices = tuple(swing.metrics_index for swing in swing_tuple)
         arrays = scorer._metric_arrays(metrics)
-
-        history_scores = scorer.prepare_history_scores(
+        history_snapshots = scorer.prepare_history_snapshots(
             swing_tuple,
             arrays,
             config.STRUCTURE_LOOKBACK,
@@ -52,25 +51,31 @@ class StructureFilter:
         total_weight = scorer._professional_total_weight
         structure_weight = scorer._professional_structure_weight
         smart_money_weight = scorer._professional_smart_money_weight
+        prepared_values = scorer._structure._prepared_values
         is_structural = self._is_structural
         score_from_batch_raw = scorer._smart_money.score_from_batch_raw
 
-        survivors: list[int] = []
-        professional_scores: dict[int, float] = {}
-
-        for index in range(1, len(swing_tuple)):
-            history_score = history_scores[index]
-            if history_score is None:
+        for index, current in enumerate(swing_tuple):
+            if index == 0:
                 continue
 
+            snapshot = history_snapshots[index]
+            if snapshot is None:
+                continue
+
+            metric_index = metric_indices[index]
             (
-                _price,
-                _structural_size,
-                _duration_score,
-                _volume_score,
-                _spread_score,
+                price,
+                structural_size,
+                duration_score,
+                volume_score,
+                spread_score,
                 structure_overall,
-            ) = history_score
+            ) = prepared_values(
+                snapshot=snapshot,
+                volume=float(arrays[4][metric_index]),
+                spread=float(arrays[5][metric_index]),
+            )
 
             smart_money_score = float(raw_smart_money[-1][index])
             if total_weight <= 0:
@@ -87,42 +92,12 @@ class StructureFilter:
             if not is_structural(professional_overall):
                 continue
 
-            survivors.append(index)
-            professional_scores[index] = professional_overall
-
-        if not survivors:
-            return structural
-
-        history_snapshots = scorer.prepare_history_snapshots(
-            swing_tuple,
-            arrays,
-            config.STRUCTURE_LOOKBACK,
-            indices_to_build=set(survivors),
-        )
-
-        for index in survivors:
-            current = swing_tuple[index]
-            snapshot = history_snapshots[index]
-            if snapshot is None:
-                continue
-
-            (
-                price,
-                structural_size,
-                duration_score,
-                volume_score,
-                spread_score,
-                structure_overall,
-            ) = history_scores[index]
-
-            metric_index = metric_indices[index]
             smart_money = score_from_batch_raw(
                 raw_smart_money,
                 index,
                 source_index=int(metric_index),
                 include_components=True,
             )
-            professional_overall = professional_scores[index]
 
             structure_score = StructuralSwingScore(
                 price=price,
